54e2e6dbf4c298010dc8123c0d44d475675e9e97
yuxs <14122291+yuxes@user.noreply.gitee.com>
2024-04-01 19:14:09 +0800

    Signed-off-by: yuxs <14122291+yuxes@user.noreply.gitee.com>

---
+++ demo/src/main/java/com/yuxs/demo/demo/Guid.java
@@ -0,0 +1,9 @@
+package com.yuxs.demo.demo;
+
+import java.util.UUID;
+public class Guid {
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+    }
+}
